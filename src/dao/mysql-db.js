4d0e7340c005c2313648b7d2d48d00bde234ddf7
dymok <dymokilan@gmail.com>
2024-05-13 20:02:03 +0200

    removed hardcoding from database connection

--- src/dao/mysql-db.js
+++ src/dao/mysql-db.js
@@ -11,9 +11,9 @@ tracer.setLevel(process.env.LOG_LEVEL)
 const dbConfig = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    user: 'root',
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'share-a-meal',
+    database: process.env.DB_DATABASE,
 
     connectionLimit: 10,
     waitForConnections: true,
