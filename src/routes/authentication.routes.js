3320b3e9e885b0d120675f2fefc1f2c7b0629346
dymok <dymokilan@gmail.com>
2024-05-14 21:10:30 +0200

    added auth tests

--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -1,80 +1,65 @@
-//
-// Authentication routes
-//
-const assert = require('assert')
-const jwt = require('jsonwebtoken')
-const jwtSecretKey = require('../util/config').secretkey
-const routes = require('express').Router()
-const authController = require('../controllers/authentication.controller')
-const logger = require('../util/logger')
+const assert = require('assert');
+const jwt = require('jsonwebtoken');
+const jwtSecretKey = require('../util/config').secretkey;
+const routes = require('express').Router();
+const authController = require('../controllers/authentication.controller');
+const logger = require('../util/logger');
 
-//
-//
-//
 function validateLogin(req, res, next) {
-    // Verify that we receive the expected input
     try {
-        assert(
-            typeof req.body.emailAdress === 'string',
-            'email must be a string.'
-        )
-        assert(
-            typeof req.body.password === 'string',
-            'password must be a string.'
-        )
-        next()
+        if (!req.body.emailAdress) {
+            throw new Error('missing emailAdress');
+        }
+        if (!req.body.password) {
+            throw new Error('missing password');
+        }
+        if (typeof req.body.emailAdress !== 'string') {
+            throw new Error('emailAdress must be a string');
+        }
+        if (typeof req.body.password !== 'string') {
+            throw new Error('password must be a string');
+        }
+        next();
     } catch (ex) {
-        next({
-            status: 409,
-            message: ex.toString(),
+        res.status(400).json({
+            status: 400,
+            message: ex.message,
             data: {}
-        })
+        });
     }
 }
 
-//
-//
-//
 function validateToken(req, res, next) {
-    logger.info('validateToken called')
-    logger.trace('Headers:', req.headers)
-    // The headers should contain the authorization-field with value 'Bearer [token]'
-    const authHeader = req.headers.authorization
+    logger.info('validateToken called');
+    logger.trace('Headers:', req.headers);
+    const authHeader = req.headers.authorization;
     if (!authHeader) {
-        logger.warn('Authorization header missing!')
-        next({
+        logger.warn('Authorization header missing!');
+        res.status(401).json({
             status: 401,
             message: 'Authorization header missing!',
             data: {}
-        })
+        });
     } else {
-        // Strip the word 'Bearer ' from the headervalue
-        const token = authHeader.substring(7, authHeader.length)
-
+        const token = authHeader.substring(7, authHeader.length);
         jwt.verify(token, jwtSecretKey, (err, payload) => {
             if (err) {
-                logger.warn('Not authorized')
-                next({
+                logger.warn('Not authorized');
+                res.status(401).json({
                     status: 401,
                     message: 'Not authorized!',
                     data: {}
-                })
+                });
             }
             if (payload) {
-                logger.debug('token is valid', payload)
-                /**
-                 * User heeft toegang.
-                 * BELANGRIJK! Voeg UserId uit payload toe aan request,
-                 * zodat die voor ieder volgend endpoint beschikbaar is.
-                 * Je hebt dan altijd toegang tot de userId van de ingelogde gebruiker.
-                 */
-                req.userId = payload.userId
-                next()
+                logger.debug('token is valid', payload);
+                req.userId = payload.userId;
+                next();
             }
-        })
+        });
     }
 }
 
-routes.post('/login', validateLogin, authController.login)
+routes.post('/login', validateLogin, authController.login);
 
-module.exports = { routes, validateToken }
+module.exports = { routes, validateToken };
--- src/services/authentication.service.js
+++ src/services/authentication.service.js
@@ -1,85 +1,65 @@
-//
-// Authentication controller
-//
-const jwt = require('jsonwebtoken')
-const db = require('../dao/mysql-db')
-// const validateEmail = require('../util/emailvalidator')
-const logger = require('../util/logger')
-const jwtSecretKey = require('../util/config').secretkey
+const jwt = require('jsonwebtoken');
+const db = require('../dao/mysql-db');
+const logger = require('../util/logger');
+const jwtSecretKey = require('../util/config').secretkey;
 
-const authController = {
+const authService = {
     login: (userCredentials, callback) => {
-        logger.debug('login')
+        logger.debug('login');
 
         db.getConnection((err, connection) => {
             if (err) {
-                logger.error(err)
-                callback(err.message, null)
+                logger.error(err);
+                callback(err, null);
             }
             if (connection) {
-                // 1. Kijk of deze useraccount bestaat.
+                // 1. Check if the user account exists.
                 connection.query(
                     'SELECT `id`, `emailAdress`, `password`, `firstName`, `lastName` FROM `user` WHERE `emailAdress` = ?',
                     [userCredentials.emailAdress],
-                    (err, rows, fields) => {
-                        connection.release()
+                    (err, rows) => {
+                        connection.release();
                         if (err) {
-                            logger.error('Error: ', err.toString())
-                            callback(error.message, null)
-                        }
-                        if (rows) {
-                            // 2. Er was een resultaat, check het password.
-                            if (
-                                rows &&
-                                rows.length === 1 &&
-                                rows[0].password == userCredentials.password
-                            ) {
-                                logger.debug(
-                                    'passwords DID match, sending userinfo and valid token'
-                                )
-                                // Extract the password from the userdata - we do not send that in the response.
-                                const { password, ...userinfo } = rows[0]
-                                // Create an object containing the data we want in the payload.
-                                const payload = {
-                                    userId: userinfo.id
-                                }
+                            logger.error('Error: ', err.toString());
+                            callback({ status: 500, message: err.message }, null);
+                        } else if (rows && rows.length === 1) {
+                            // 2. There was a result, check the password.
+                            if (rows[0].password === userCredentials.password) {
+                                logger.debug('Passwords DID match, sending userinfo and valid token');
+                                const { password, ...userinfo } = rows[0];
+                                const payload = { userId: userinfo.id };
 
                                 jwt.sign(
                                     payload,
                                     jwtSecretKey,
                                     { expiresIn: '12d' },
                                     (err, token) => {
-                                        logger.info(
-                                            'User logged in, sending: ',
-                                            userinfo
-                                        )
-                                        callback(null, {
-                                            status: 200,
-                                            message: 'User logged in',
-                                            data: { ...userinfo, token }
-                                        })
+                                        if (err) {
+                                            logger.error('Error: ', err.toString());
+                                            callback({ status: 500, message: err.message }, null);
+                                        } else {
+                                            logger.info('User logged in, sending: ', userinfo);
+                                            callback(null, {
+                                                status: 200,
+                                                message: 'User logged in',
+                                                data: { ...userinfo, token }
+                                            });
+                                        }
                                     }
-                                )
+                                );
                             } else {
-                                logger.debug(
-                                    'User not found or password invalid'
-                                )
-                                callback(
-                                    {
-                                        status: 409,
-                                        message:
-                                            'User not found or password invalid',
-                                        data: {}
-                                    },
-                                    null
-                                )
+                                logger.debug('Password invalid');
+                                callback({ status: 409, message: 'User not found or password invalid' }, null);
                             }
+                        } else {
+                            logger.debug('User not found');
+                            callback({ status: 404, message: 'User not found' }, null);
                         }
                     }
-                )
+                );
             }
-        })
-    },
-}
+        });
+    }
+};
 
-module.exports = authController
+module.exports = authService;
